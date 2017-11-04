85a084415a3127dcba0b7c9005bf913464ea52e9
haeyoungJoo <haeyoungJoo@172.19.94.30>
2017-11-04 10:16:22 +0900

    프런트 컨트롤러 변경

--- MiniFramWork/src/spms/servlets/DispatcherServlet.java
+++ MiniFramWork/src/spms/servlets/DispatcherServlet.java
@@ -23,60 +23,53 @@ import spms.vo.Member;
 @SuppressWarnings("serial")
 @WebServlet("*.do")
 public class DispatcherServlet extends HttpServlet {
+
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html; charset=UTF-8");
 		String servletPath = request.getServletPath();
-		
+
 		try {
 			ServletContext sc = this.getServletContext();
 			HashMap<String, Object> model = new HashMap<String, Object>();
-			model.put("memberDao", sc.getAttribute("memberDao"));
-			String pageControllerPath = null;
-			Controller pageController = null;
 
-			if ("/member/list.do".equals(servletPath)) {
-				pageController = new MemberListController();
-			} else if ("/member/add.do".equals(servletPath)) {
-				pageController = new MemberAddController();
+			String pageControllerPath = null;
+			Controller pageController = (Controller) sc.getAttribute(servletPath);
+
+			if ("/member/add.do".equals(servletPath)) {
+
 				if (request.getParameter("email") != null) {
-					model.put("member", new Member()
-							.setEmail(request.getParameter("email"))
-							.setPassword(request.getParameter("password"))
-							.setName(request.getParameter("name")));
+					model.put("member", new Member().setEmail(request.getParameter("email"))
+							.setPassword(request.getParameter("password")).setName(request.getParameter("name")));
 				}
-				
+
 			} else if ("/member/update.do".equals(servletPath)) {
-				
-				pageController= new MemberUpdateController();
+
 				if (request.getParameter("email") == null) {
-					model.put("no",Integer.parseInt(request.getParameter("no")));	
-				}
-				else{
-					model.put("member",  new Member().setNo(Integer.parseInt(request.getParameter("no")))
+					model.put("no", Integer.parseInt(request.getParameter("no")));
+				} else {
+					model.put("member", new Member().setNo(Integer.parseInt(request.getParameter("no")))
 							.setEmail(request.getParameter("email")).setName(request.getParameter("name")));
 				}
 			} else if ("/member/delete.do".equals(servletPath)) {
-				pageController = new MemberDeleteController();
+
 				model.put("no", Integer.parseInt(request.getParameter("no")));
-			
+
 			} else if ("/auth/login.do".equals(servletPath)) {
-				pageController = new LogInController();
-				if( request.getParameter("email")!=null) {
-					
-					model.put("session",request.getSession());
-					model.put("email",request.getParameter("email"));
-					model.put("password",request.getParameter("password"));
+
+				if (request.getParameter("email") != null) {
+
+					model.put("session", request.getSession());
+					model.put("email", request.getParameter("email"));
+					model.put("password", request.getParameter("password"));
 				}
-				
+
 			} else if ("/auth/logout.do".equals(servletPath)) {
-				pageController = new LogOutController();
-				model.put("session",request.getSession());
+
+				model.put("session", request.getSession());
 			}
-			
 			String viewUrl = pageController.execute(model);
-			
 			for (String key : model.keySet()) {
 				request.setAttribute(key, model.get(key));
 			}
